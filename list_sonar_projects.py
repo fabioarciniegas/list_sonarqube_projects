d4cbdf9bbe4d012cffb66955e4f806e72f86ff98
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2024-02-19 09:36:28 -0800

    fixed pending style todos, committed json with results for ease

--- list_sonar_projects.py
+++ list_sonar_projects.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import json
+import sys
 
 list_components_url = "https://dssonar.trendmicro.com/api/components/search?qualifiers=TRK"
 show_components_url = "https://dssonar.trendmicro.com/api/components/show"
@@ -13,16 +15,15 @@ while pages_left:
     page += 1
     # Sonarqube expects basic authentication of the form ~~ Basic base64(user:pwd)
     # with the token as user and no password
-    # TODO: f string
-    projects = requests.get(list_components_url + "&p=" + str(page),
+    projects = requests.get(f"%s&p=%s" %(list_components_url,str(page)),
                             headers={'Authorization': f"Basic %s" % token})
     components = projects.json()
     comps_in_page = len(components['components'])
-    print(f"Page {page} has {comps_in_page} components")
+    print(f"Page {page} has {comps_in_page} components",file=sys.stderr)
     for c in components['components']:
         details = requests.get(show_components_url + "?component=" + c['key'],
                                headers={'Authorization': f"Basic %s" % token})
         github_repo = details.json()['component']['description'] if 'description' in details.json()['component'].keys()  else ""
         results.append({"name":c['key'],"github_repo":github_repo})
     pages_left = comps_in_page > 0
-print(results)
+print(json.dumps(results))
